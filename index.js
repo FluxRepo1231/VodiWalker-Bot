736d3a32859c9dc7a852f37c56d2a0e42f6db212
FOS <panelfluxv@gmail.com>
2024-10-13 19:56:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
 });
 
 const statusMessages = ["", "Offline"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusTypes = [ '', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
